90db6aae73eec17c67868a961dadd90fe99fbeb7
endrigobigi <endrigo.bigi@gmx.de>
2020-10-01 08:20:24 +0200

    Project created

---
+++ src/test/java/de/endrigobigi/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.endrigobigi.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
