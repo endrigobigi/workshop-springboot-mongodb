863428379010ecd0a63f70a801658d1539482b02
endrigobigi <endrigo.bigi@gmx.de>
2020-10-03 15:16:48 +0200

    User - findById

---
+++ src/main/java/de/endrigobigi/workshopmongo/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,36 @@
+package de.endrigobigi.workshopmongo.resources.exception;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import de.endrigobigi.workshopmongo.services.exception.ObjectNotFoundException;
+
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		String error = "Object not found.";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	/*
+	@ExceptionHandler(DatabaseException.class) //anotation que intercepta qualquer ocorrência de erro e trata conforme método implementado
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+		String error = "Database error.";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	*/
+
+}
