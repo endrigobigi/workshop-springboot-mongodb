9777aa925ae319b1950ca5de8a47cecc36267191
endrigobigi <endrigo.bigi@gmx.de>
2020-10-04 10:12:30 +0200

    User delete

--- src/main/java/de/endrigobigi/workshopmongo/services/UserService.java
+++ src/main/java/de/endrigobigi/workshopmongo/services/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import de.endrigobigi.workshopmongo.domain.User;
@@ -23,11 +24,20 @@ public class UserService {
 	
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado."));
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found."));
 	}
 	
 	public User insert(User obj) {
 		return repo.insert(obj);
+	}
+	
+	public void delete(String id) {
+		try {
+		repo.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ObjectNotFoundException("Object not found.");
+		}
 	}
 	
 	public User fromDTO(UserDTO objDto) {
