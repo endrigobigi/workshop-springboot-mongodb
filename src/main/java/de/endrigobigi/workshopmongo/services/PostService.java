cc02faf6f27e14d9fdae69a064c07e7ebf86bfe2
endrigobigi <endrigo.bigi@gmx.de>
2020-10-05 20:02:11 +0200

    Getting posts by id

---
+++ src/main/java/de/endrigobigi/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package de.endrigobigi.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import de.endrigobigi.workshopmongo.domain.Post;
+import de.endrigobigi.workshopmongo.repository.PostRepository;
+import de.endrigobigi.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found."));
+	}
+}
